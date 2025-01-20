89d664da73314da903e614e0120dc36fdfb6793b
MirSiam10 <mir10siam@gmail.com>
2025-01-20 20:00:46 +0600

    age

---
+++ src/Java/Conditions/Ifelse.java
@@ -0,0 +1,15 @@
+public static void main(String args[]){
+
+    Scanner sc = new Scanner(System.in);
+
+    int age = sc.nextInt();
+
+    if (18<age){
+        System.out.println("Adult");
+    }
+
+    else {
+        System.out.println("Not Adult");
+}
+
+}
