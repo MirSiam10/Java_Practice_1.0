f8800a1da9ac12b01855d66d986ef3a7e89c1d11
MirSiam10 <mir10siam@gmail.com>
2025-01-21 21:43:47 +0600

    New Lesson

---
+++ src/Java/Conditions/oddOrEven.java
@@ -0,0 +1,18 @@
+package Java.Conditions;
+
+import java.util.Scanner;
+
+public class oddOrEven {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int Number = sc.nextInt();
+
+        if (Number%2 == 0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Even Number");
+        }
+    }
+}
